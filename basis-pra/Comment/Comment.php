642ca15fdf5b9848bd31a1bb871dacdbf0eb95cd
kancheng <zxdfgcv@gmail.com>
2017-04-11 15:05:09 +0800

    Upload ch3 book exercise 1 & 2

---
+++ basis-pra/Comment/Comment.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+			<title> My First PHP</title>
+	</head>
+	<body>
+		<?php 
+		# comment 1
+
+		// comment 2
+
+		/* comment 3 */
+			phpinfo();
+	 	?>
+	</body>
+</html>
---
+++ basis-pra/HelloWorld/HelloWorld.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+			<title> My First PHP</title>
+	</head>
+	<body>
+		<?php 
+			echo "Hi! Hello, World ! ";
+	 	?>
+	</body>
+</html>
