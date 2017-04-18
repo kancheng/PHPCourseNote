95450be73bac81a61019b2d6e1884c5975ef4c88
kancheng <zxdfgcv@gmail.com>
2017-04-19 04:23:27 +0800

    Upload ch3 book exercise 7

---
+++ basis-pra/stringHeredoc/stringHeredoc.php
@@ -0,0 +1,20 @@
+﻿<html>
+	<head>
+			<title>PHP Practice</title>
+	</head>
+	<body>
+		<?php 
+			$myLanguage = "PHP";
+
+			/* heredoc 表示字串 */
+
+			$showStr = <<<Msg
+						目前用的程式語言 : $myLanguage <br />
+						Haoye："It's easy, It's good." 						
+Msg;
+
+			echo $showStr; 
+
+	 	?>
+	</body>
+</html>
