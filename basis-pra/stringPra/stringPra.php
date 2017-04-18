f9c6a4589b15f779d33df25909a0847632aaf35b
kancheng <zxdfgcv@gmail.com>
2017-04-19 03:58:28 +0800

    Upload ch3 book exercise 6

---
+++ basis-pra/stringPra/stringPra.php
@@ -0,0 +1,28 @@
+﻿<html>
+	<head>
+			<title>PHP Practice</title>
+	</head>
+	<body>
+		<?php 
+			/* 1 */
+			echo '1. 這是一個單引字串 <br />';
+
+			/* 2 */
+			echo '2. How are you? I\'m fine, thank you. <br />';
+
+			/* 3 */
+			echo '3. PHP 安裝路徑在 C:\\PHP\\ 中 <br />';
+
+			/* 4 */
+			echo "4. 這是一個雙引字串 <br />";
+
+			/* 5 */
+			echo "5. 在雙引號中單引號不需要 '跳脫字元' ，但是雙引號必須要用 \"跳脫字元\" 。<br />";
+
+			/* 6 */
+			$myLanguage = "PHP";
+			echo '6-1. 單引號視為字串變數處理 $myLanguage <br /> ';
+			echo "6-2. 雙引號視為變數編譯 $myLanguage ";
+	 	?>
+	</body>
+</html>
