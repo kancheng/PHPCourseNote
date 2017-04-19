aa690edce1cb69bafbc6cddaba279039042a15ee
kancheng <zxdfgcv@gmail.com>
2017-04-20 02:20:13 +0800

    Upload ch4 exercise practice 2

---
+++ program-pra/MultiplicationTable/MultiplicationTable.php
@@ -0,0 +1,13 @@
+﻿<?php
+	$a = 1;
+	$b = 1;
+
+	echo 'Multiplication Table <br /><br />';
+	for ($a = 1; $a <= 16; $a++){
+		for($b = 1; $b <= 16; $b++){
+			echo '  '. $a . ' * ' . $b . ' = ' . $a * $b . '<br />';
+		}
+		echo ' <br /> ';
+	}
+
+?>
