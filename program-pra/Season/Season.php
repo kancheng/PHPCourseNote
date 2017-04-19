22f3f1e236bbd2539025ebcf0f08a031722ad446
kancheng <zxdfgcv@gmail.com>
2017-04-20 02:00:50 +0800

    Upload ch4 exercise practice 1

---
+++ program-pra/Season/Season.php
@@ -0,0 +1,15 @@
+﻿<?php 
+
+	$sea = 8;
+
+	if ($sea >= 1 && $sea < 4){
+		echo '春季';
+	} elseif ($sea >= 4 && $sea < 7){
+		echo '夏季';
+	} elseif ($sea >= 7 && $sea < 10){
+		echo '秋季';
+	} elseif ($sea >= 10 && $sea < 13){
+		echo '冬季';
+	}
+
+?>
