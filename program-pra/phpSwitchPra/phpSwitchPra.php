9f1f1f0e546dd7179967c116bbac51018e85d648
kancheng <zxdfgcv@gmail.com>
2017-04-20 01:06:58 +0800

    Upload ch4 book exercise 4

---
+++ program-pra/phpSwitchPra/phpSwitchPra.php
@@ -0,0 +1,22 @@
+﻿<?php 
+
+	$direction = "南";
+	switch ($direction){
+		case "東" :
+			echo "我要往東走";
+		break;
+		case "南" :
+			echo "我要往南走";
+		break;
+		case "西" :
+			echo "我要往西走";
+		break;
+		case "北" :
+			echo "我要往北走";
+		break;
+		default :
+			echo "不知道要往哪走";
+		break;
+	}
+
+?>
