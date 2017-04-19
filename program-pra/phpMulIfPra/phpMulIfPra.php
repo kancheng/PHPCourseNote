7348036e713e0da24c89ab3e64f9862bd7803457
kancheng <zxdfgcv@gmail.com>
2017-04-20 00:59:13 +0800

    Upload ch4 book exercise 3

---
+++ program-pra/phpMulIfPra/phpMulIfPra.php
@@ -0,0 +1,17 @@
+﻿<?php 
+
+	$score = 85;
+
+	if ($score >= 60 && $score < 70){
+		echo '丙等';
+	} elseif ($score >= 70 && $score < 80){
+		echo '乙等';
+	} elseif ($score >= 80 && $score < 90){
+		echo '甲等';
+	} elseif ($score >= 90 && $score < 100){
+		echo '優等';
+	} else {
+		echo '不及格';
+	}
+
+?>
