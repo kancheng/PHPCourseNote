6c50bd046257b8590fb4c3d4b652059bc19c1238
kancheng <zxdfgcv@gmail.com>
2017-04-20 22:23:24 +0800

    Upload ch5 book exercise 7

---
+++ function-pra/phpGVLVPra/phpGVLVPra.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+
+	$msg = "This is Global Variable <br />";
+
+	function showMsg(){
+		$msg = "This is Local Variable <br />";
+		echo $msg;
+	}
+
+	echo $msg;
+	showMsg();
+	echo $msg;
+?>
