5ded126bfffe18b6fb059614b89bc1e85cd9ab43
kancheng <zxdfgcv@gmail.com>
2017-04-20 22:16:01 +0800

    Upload ch5 book exercise 6

---
+++ function-pra/phpShowClassData/phpShowClassData.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+	function showData(){
+		$i = func_num_args();
+
+		if ( $i == 0) {
+			echo "沒有指定學生資料 ! <br />";
+
+		} else {
+			echo "本班學生有 : ";
+			$student = func_get_args();
+			foreach ($student as $data){
+				echo $data . ", ";
+			}
+			echo "最後一個為 : " . func_get_arg($i - 1) . "。 <br />";
+		}
+	}
+
+	showData();
+	showData("haoye", "jhen", "jeff", "jun");
+?>
