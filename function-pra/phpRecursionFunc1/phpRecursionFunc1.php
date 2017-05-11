12bdc7ef8fa107e4d23f549cac64a982adce7f48
kancheng <zxdfgcv@gmail.com>
2017-05-11 20:51:28 +0800

    Upload ch5 book exercise 10

---
+++ function-pra/phpRecursionFunc1/phpRecursionFunc1.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+	function showResult($Num){
+		if( $Num >= 0 ) {
+
+			if($Num == 0) {
+				return 1;
+			} else {
+				$resultNum = 1;
+				for($n = $Num; $n > 0; $n--) {
+					$resultNum *= $n;
+				}
+				return $resultNum;
+			}
+
+		}
+	}
+
+	echo "5 的階層為 (by Recursion) : " . showResult(5);
+?>
