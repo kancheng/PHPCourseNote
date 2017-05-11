04f1524180c70d2eeefb783da5cde0a0aa7d9a9a
kancheng <zxdfgcv@gmail.com>
2017-05-11 21:15:59 +0800

    Upload ch5 book exercise 12

---
+++ function-pra/phpRandFunc/phpRandFunc.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+srand((double)microtime() * 1000000);
+$randval = rand(0,5);
+switch ($randval) {
+
+	case "0":
+		echo "Hi! Haoye.";
+		break;
+
+	case "1":
+		echo "Hi! KanCheng.";
+		break;
+
+	case "2":
+		echo "Hi! Jun.";
+		break;
+
+	case "3":
+		echo "Hi! Jeff.";
+		break;
+
+	case "4":
+		echo "Hi! Jhen.";
+		break;
+
+	case "5":
+		echo "Hi! Kan.";
+		break;
+
+}
+
+?>
