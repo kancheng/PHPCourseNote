e575753dfde3a0cabd4a6f49637a9af675d0ac25
kancheng <zxdfgcv@gmail.com>
2017-05-11 20:39:50 +0800

    Upload ch5 book exercise 9

---
+++ function-pra/phpTestFunc/phpTestFunc.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+	function testfun1(){
+		echo "目前的自訂函式 1 <br />";
+	}
+
+	function testfun2(){
+		echo "目前的自訂函式 2 <br />";
+	}
+	$callTest = "testfun1";
+	$callTest();
+	$callTest = "testfun2";
+	$callTest();
+?>
