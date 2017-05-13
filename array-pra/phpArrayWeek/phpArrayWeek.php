08fca91f6fae05fd06e964ef96efae34cfdf08d9
kancheng <zxdfgcv@gmail.com>
2017-05-13 11:34:37 +0800

    Upload ch6 book exercise 1

---
+++ array-pra/phpArrayWeek/phpArrayWeek.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+$weekArray[] = 'Sunday';
+$weekArray[] = 'Monday';
+$weekArray[] = 'Tuesday';
+$weekArray[] = 'Wednesday';
+$weekArray[] = 'Thursday';
+$weekArray[] = 'Friday';
+$weekArray[] = 'Saturday';
+
+	for( $i=0; $i<=6; $i++){
+		echo $weekArray[$i] . "<br />";
+	}
+
+?>
