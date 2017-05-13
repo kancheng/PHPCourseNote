3a75fd35536e2d71e965fe2018345b963e7714a6
kancheng <zxdfgcv@gmail.com>
2017-05-14 04:29:33 +0800

    Upload ch6 book exercise 14

---
+++ array-pra/phpNatSortArray/phpNatSortArray.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+echo "<h1>測試陣列排序 </h1><hr />";
+
+echo "<h2> sort() : </h2>";
+
+$testArrary = array( "img1", "img2", "img11");
+
+sort($testArrary);
+
+print_r($testArrary);
+
+echo "<br />";
+echo "<h2> natsort() : </h2>";
+
+natsort($testArrary);
+
+print_r($testArrary);
+
+?>
