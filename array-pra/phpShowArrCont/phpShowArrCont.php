7fe2200b5bb95b95c7851860d523dee878cc8278
kancheng <zxdfgcv@gmail.com>
2017-05-13 22:38:03 +0800

    Upload ch6 book exercise 11

---
+++ array-pra/phpShowArrCont/phpShowArrCont.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+echo "<h1>測試陣列資料 </h1><hr /><br />";
+
+$testArray = array( "甲", "乙", "丙", "丁");
+
+echo "1. print_r() 結果 : ";
+print_r($testArray);
+echo "<br /><br /> 2. var_dump() 結果 : ";
+var_dump($testArray);
+
+?>
