228d98bedc1ff346dfc0c9846bc586f0e71c08e0
kancheng <zxdfgcv@gmail.com>
2017-05-13 11:50:20 +0800

    Upload ch6 book exercise 2

---
+++ array-pra/phpArrayManData/phpArrayManData.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+$myArray['myName'] = 'Haoye';
+$myArray['myHeight'] = 173;
+$myArray['myWeight'] = 95;
+
+echo $myArray['myName'] . " 的自我介紹 : <br />" .
+	"大家好我的名字叫做 " . $myArray['myName'] . " .<br />" .
+	"身高 : " . $myArray['myHeight'] . " cm .<br />" .
+	"體重 : " . $myArray['myWeight'] . " kg .<br />";
+
+?>
