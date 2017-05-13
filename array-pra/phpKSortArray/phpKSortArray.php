3c96bd9285521831697433d803eff8cc85496b03
kancheng <zxdfgcv@gmail.com>
2017-05-14 00:54:25 +0800

    Upload ch6 book exercise 13

---
+++ array-pra/phpKSortArray/phpKSortArray.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+echo "<h1>測試陣列排序 </h1><hr />";
+
+echo "<h2> ksort() : </h2>";
+
+$fruits = array( 
+	"d" => "lemon", 
+	"a" => "orange", 
+	"b" => "banana", 
+	"c" => "apple");
+
+ksort($fruits);
+
+foreach ($fruits as $key => $val) {
+	echo "$key = $val <br />";
+}
+
+?>
