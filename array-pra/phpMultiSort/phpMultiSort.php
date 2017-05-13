724faf2145d54cb1cd0523ef926ed391a855d2c0
kancheng <zxdfgcv@gmail.com>
2017-05-14 04:51:56 +0800

    Upload ch6 book exercise 16

---
+++ array-pra/phpMultiSort/phpMultiSort.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+echo "<div style = 'text-align:center'><h1> Multisort() </h1><hr />";
+
+$a = array( 20, 25, 18, 34, 20, 45);
+
+$b = array( '礦泉水','泡麵','洋芋片','餅乾','八寶粥','罐頭');
+
+
+array_multisort( $a, SORT_DESC, $b, SORT_ASC);
+
+for ($i = 0; $i < count($a); $i++) { 
+	echo $b[$i] . ' 售價 $' . $a[$i] . '<br />';
+}
+
+echo "</div>";
+?>
