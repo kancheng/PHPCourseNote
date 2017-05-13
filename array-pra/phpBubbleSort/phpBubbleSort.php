ac2476731b9ab9f7115c9db380f41309938c9904
kancheng <zxdfgcv@gmail.com>
2017-05-14 04:42:31 +0800

    Upload ch6 book exercise 15

---
+++ array-pra/phpBubbleSort/phpBubbleSort.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+echo "<div style = 'text-align:center'><h1> Bubble Sort </h1><hr />";
+
+function cmp( $a, $b) {
+	if ( $a == $b){
+		/* 相等回傳 0 */
+		return 0;
+	}
+
+	/* 若小於回傳 -1，否則回傳 1 */
+	return ($a < $b) ? -1 : 1;
+}
+
+$aNum = array( 3, 2, 5, 6, 1, 4, 7);
+
+foreach ($aNum as $key => $value) {
+	echo "$key : $value <br />";
+}
+echo "</div>";
+?>
