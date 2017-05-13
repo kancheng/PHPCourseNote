cb1831fef57c2d8cc3765a709439b20a50f5f871
kancheng <zxdfgcv@gmail.com>
2017-05-14 00:42:41 +0800

    Upload ch6 book exercise 12

---
+++ array-pra/phpSortArray/phpSortArray.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+echo "<h1>測試陣列排序 </h1><hr /><br />";
+
+echo "<h2> 1. sort() : </h2><br />";
+
+$fruits1 = array( 
+	"d" => "lemon", 
+	"a" => "orange", 
+	"b" => "banana", 
+	"c" => "apple");
+
+sort($fruits1);
+
+foreach ($fruits1 as $key => $val) {
+	echo "$key = $val <br />";
+}
+
+echo "<br /><hr /><h2> 2. asort() : </h2><br />";
+
+$fruits2 = array( 
+	"d" => "lemon", 
+	"a" => "orange", 
+	"b" => "banana", 
+	"c" => "apple");
+
+asort($fruits2);
+
+foreach ($fruits2 as $key => $val) {
+	echo "$key = $val <br />";
+}
+
+?>
