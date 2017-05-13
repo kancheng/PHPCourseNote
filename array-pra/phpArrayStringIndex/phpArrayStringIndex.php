2519dd2c724992106d8aaef993605629e5c22ec9
kancheng <zxdfgcv@gmail.com>
2017-05-13 14:27:14 +0800

    Upload ch6 book exercise 4

---
+++ array-pra/phpArrayStringIndex/phpArrayStringIndex.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+$myArray = array('myName' => 'Haoye', 'myHeight' => 173, 'myWeight' => 95);
+
+echo $myArray['myName'] . " 的自我介紹 : <br />" .
+	"大家好我的名字叫做 " . $myArray['myName'] . " .<br />" .
+	"身高 : " . $myArray['myHeight'] . " cm .<br />" .
+	"體重 : " . $myArray['myWeight'] . " kg .<br />";
+
+?>
