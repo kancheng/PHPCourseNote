0cfe2ea0d1294d88d8497ccf5d060f9adc5741a7
kancheng <zxdfgcv@gmail.com>
2017-05-13 19:36:18 +0800

    Upload ch6 book exercise 9

---
+++ array-pra/phpFEWeek/phpFEWeek.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+$weekArray = array( '星期日'=>'Sunday', '星期一'=>'Monday',
+	'星期二'=>'Tuesday', '星期三'=>'Wednesday', '星期四'=>'Thursday',
+	'星期五'=>'Friday', '星期六'=>'Saturday'); 
+
+	foreach( $weekArray as $cweek => $eweek){
+		echo "中文名稱 ".$cweek.", 其英文為 ".$eweek."<br />";
+	}
+
+?>
