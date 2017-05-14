801d0e3735c68247671425a1b3e9629db879591b
kancheng <zxdfgcv@gmail.com>
2017-05-14 23:53:08 +0800

    Upload ch6 exercise practice 2

---
+++ array-pra/MultiTableFTFeach/MultiTableFTFeach.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+echo "<div style = 'text-align:center'><h1> Multiplication Table </h1><hr />";
+
+$Save = array();
+
+	for ($a = 1; $a <= 16; $a++){
+		for($b = 1; $b <= 16; $b++){
+			if ($b == 16) {
+				$Save[$a][$b] = $a . ' * ' . $b . ' = ' . $a * $b . '<br /><br />';
+			} else {
+				$Save[$a][$b] = $a . ' * ' . $b . ' = ' . $a * $b . '<br />';
+			}
+		}
+	}
+
+$SNam = count($Save);
+
+foreach ($Save as $SNam1) {
+
+	foreach ($SNam1 as $SNam2) {
+		echo $SNam2;
+	}
+}
+
+echo "</div>";
+?>
