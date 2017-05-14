b46483973d7172023d0654e143d2216339714294
kancheng <zxdfgcv@gmail.com>
2017-05-15 00:47:49 +0800

    Upload ch7 book exercise 2

---
+++ string-pra/phpStripTags/phpStripTags.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1> Hi !</h1><hr />";
+
+$showStr = " 你學 <strong>PHP</strong><br /> 我學 <strong>PHP</strong><br /> 大家學 <strong>PHP</strong>!!!";
+
+echo $showStr . "<hr />";
+
+echo strip_tags( $showStr,'<br />');
+
+echo "</div>";
+?>
+
