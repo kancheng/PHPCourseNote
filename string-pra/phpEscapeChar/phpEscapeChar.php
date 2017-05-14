1c55015b6e7d3995b9b26a2114355b199ed5d3b9
kancheng <zxdfgcv@gmail.com>
2017-05-15 01:52:39 +0800

    Upload ch7 book exercise 3

---
+++ string-pra/phpEscapeChar/phpEscapeChar.php
@@ -0,0 +1,18 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Escape Char</h1><hr />";
+
+$showStr = "('HaoyePages'[網址])https://www.kancheng.com.tw/?about=true";
+
+echo $showStr . "<hr />";
+echo quotemeta($showStr) . "<hr />";
+echo addslashes($showStr) . "<hr />";
+echo addcslashes($showStr,"'[]") . "<hr />";
+
+$showStr1 = addslashes($showStr);
+$showStr2 = addcslashes($showStr,"'[]");
+
+echo stripslashes($showStr1) . "<hr />";
+echo stripcslashes($showStr2) . "<hr />";
+echo "</div>";
+?>
+
