9318d1131e5d76b40698e9fb9b6261d9790ae3ea
kancheng <zxdfgcv@gmail.com>
2017-05-21 08:37:55 +0800

    Upload ch7 book exercise 4

---
+++ string-pra/phpSpaceProc/phpSpaceProc.php
@@ -0,0 +1,16 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Space Proc</h1><hr />";
+
+$showStr1 = "大家";
+$showStr2 = " 一起學 ";
+$showStr3 = "PHP ! ";
+
+echo $showStr1 . $showStr2 . $showStr3 . "<hr />";
+echo $showStr1 . trim($showStr2) . $showStr3 . "<hr />";
+echo $showStr1 . ltrim($showStr2) . $showStr3 . "<hr />";
+echo $showStr1 . rtrim($showStr2) . $showStr3 . "<hr />";
+echo $showStr1 . chop($showStr2) . $showStr3 ;
+
+echo "</div>";
+?>
+
