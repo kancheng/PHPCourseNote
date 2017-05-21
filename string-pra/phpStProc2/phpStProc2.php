4114c070c40db52b27a56f23ca6df90371455721
kancheng <zxdfgcv@gmail.com>
2017-05-21 08:59:34 +0800

    Upload ch7 book exercise 6

---
+++ string-pra/phpStProc2/phpStProc2.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>St Proc 2</h1><hr />";
+
+$showStr = "Genius is one percent inspiration and ninety-nine percent perspiration.";
+
+echo "天才是一分靈感加上九十九分的努力。<hr />";
+echo chunk_split( $showStr, 25, "<br />"). "<hr />";
+echo wordwrap( $showStr, 25, "<br />");
+
+echo "</div>";
+?>
+
