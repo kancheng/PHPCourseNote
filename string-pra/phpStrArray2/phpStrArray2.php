9c27180b5e9050d274e8d3ecbf182a05d07871ce
kancheng <zxdfgcv@gmail.com>
2018-12-12 13:25:49 +0800

    Upload ch7 book exercise 9

---
+++ string-pra/phpStrArray2/phpStrArray2.php
@@ -0,0 +1,13 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Array</h1><hr />";
+
+$showStr = "壹貳叁肆伍陸柒捌玖拾";
+
+$save = str_split($showStr,3);
+
+foreach ($save as $value){
+	echo $value."<br />";
+}
+echo "</div>";
+?>
+
