6aaf80a02c6595f8b822b22995f569028e80a7d2
kancheng <zxdfgcv@gmail.com>
2018-12-12 14:10:41 +0800

    Upload ch7 book exercise 11

---
+++ string-pra/phpCharSearch/phpCharSearch.php
@@ -0,0 +1,15 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Array</h1><hr />";
+
+	$showStr = "My name is Horst.";
+	echo $showStr . "<hr />";
+
+	$showArray = count_chars($showStr, 1);
+
+	foreach ($showArray as $i => $value) {
+		echo " 字元 " . chr($i) . " 出現了 $value 次。 <br />";
+	}
+
+echo "</div>";
+?>
+
