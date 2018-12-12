c782c8bee3699b124b542f98f9f0eb36c7413740
kancheng <zxdfgcv@gmail.com>
2018-12-12 12:31:36 +0800

    Upload ch7 book exercise 8

---
+++ string-pra/phpStrArray1/phpStrArray1.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Array</h1><hr />";
+
+$showStr = "站住 口令 誰";
+
+$save = explode(" ",$showStr);
+foreach ($save as $value){
+	echo $value."<br />";
+}
+echo "</div>";
+?>
+
