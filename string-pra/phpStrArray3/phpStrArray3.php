67e118b34590ca04398bec0e3421c9c4923b0a05
kancheng <zxdfgcv@gmail.com>
2018-12-12 13:32:48 +0800

    Upload ch7 book exercise 10

---
+++ string-pra/phpStrArray3/phpStrArray3.php
@@ -0,0 +1,9 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Array</h1><hr />";
+
+	$showStr = array("壹","貳","叁","肆","伍","陸","柒","捌","玖","拾");
+	echo implode(" - ", $showStr);
+
+echo "</div>";
+?>
+
