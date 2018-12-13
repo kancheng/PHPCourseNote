a346eb6c83798aaeea26d835518ff88a897e0131
kancheng <zxdfgcv@gmail.com>
2018-12-13 12:01:29 +0800

    Upload ch7 book exercise 17

---
+++ string-pra/phpFormatStr4/phpFormatStr4.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$birthday = "1999-10-30";
+
+	sscanf($birthday, "%d-%d-%d", $by, $bm, $bd);
+
+	printf( "某生日為 %d 年 %d 月 %d 日", $by, $bm, $bd);
+
+echo "</div>";
+?>
+
