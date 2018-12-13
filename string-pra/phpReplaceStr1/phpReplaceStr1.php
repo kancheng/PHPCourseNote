93509bc9fe629333e0fc64d74c73d003932638e3
kancheng <zxdfgcv@gmail.com>
2018-12-13 10:11:34 +0800

    Upload ch7 book exercise 12

---
+++ string-pra/phpReplaceStr1/phpReplaceStr1.php
@@ -0,0 +1,21 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Array</h1><hr />";
+
+	$filename = array( "a.jpg", "b.doc", "c.xls", "d.txt" );
+	$filetype = array( ".txt" => "Text File", ".doc" => "Word File", ".xls" => " Excel File", ".jpg" => "Picture File");
+
+	foreach($filename as $val){
+
+		$ft = strstr($val, ".");
+		foreach ($filetype as $name => $desc) {
+			if ($name == $ft) {
+				echo $val . " 為 " . $desc ." 副檔名是 " . substr($name, 1) . " 。 <br />" ;
+				break;
+			}
+		}
+
+	}
+
+echo "</div>";
+?>
+
