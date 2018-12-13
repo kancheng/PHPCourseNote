9087255882f59019e048efad8669e0b6dace8656
kancheng <zxdfgcv@gmail.com>
2018-12-13 11:24:14 +0800

    Upload ch7 book exercise 14

---
+++ string-pra/phpFormatStr1/phpFormatStr1.php
@@ -0,0 +1,17 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$name = "Kan";
+
+	$age = "3";
+
+	$height = "173";
+
+	$weight = "78";
+
+	printf("Hi! , My name is %s . , I am %d years old., Height : %d cm ; Weight : %d kg .",
+			$name, $age, $height, $weight);
+
+echo "</div>";
+?>
+
