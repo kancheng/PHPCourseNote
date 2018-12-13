c326ea90d9716e17352c9e3f42112259d77e6039
kancheng <zxdfgcv@gmail.com>
2018-12-13 10:48:17 +0800

    Upload ch7 book exercise 13

---
+++ string-pra/phpReplaceStr2/phpReplaceStr2.php
@@ -0,0 +1,17 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$str = "Hi! Kan, 1.";
+
+	echo  $str . "<br />";
+
+	echo strtr($str , "1", "2") . "<br />";
+
+	$replacestr = array( "Kan" => "Haoye", "1" => "2");
+	echo strtr( $str, $replacestr) . "<br />";
+	echo str_replace( "2", "3", $str);
+	echo substr_replace( $str, "Haoye", 0, 4);
+
+echo "</div>";
+?>
+
