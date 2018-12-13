0e5019132d7f030858441a31798288d08044e117
kancheng <zxdfgcv@gmail.com>
2018-12-13 17:15:09 +0800

    Upload ch7 book exercise 21

---
+++ string-pra/phpEncryption1/phpEncryption1.php
@@ -0,0 +1,18 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$encodeStr = "55chiu1030";
+
+	echo "String : " . $encodeStr . "<br />";
+
+	echo "str_rot13 : " . str_rot13($encodeStr) . "<br />";
+
+	echo "crypt : " . crypt($encodeStr, 'p-h,chiu') . "<br />";
+
+	echo "md5 : " . md5($encodeStr) . "<br />";
+
+	echo "shal : " . sha1($encodeStr);
+
+echo "</div>";
+?>
+
