2b8bea30607d233b89abbc90221478eee7889963
kancheng <zxdfgcv@gmail.com>
2018-12-13 12:20:00 +0800

    Upload ch7 book exercise 18

---
+++ string-pra/phpFormatStrPath/phpFormatStrPath.php
@@ -0,0 +1,10 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$queryString = "name=haoye&age=26&height=173&weight=78";
+	parse_str($queryString);
+
+	printf("Name : %s , Age : %d , Height : %d , Weight : %d", $name, $age, $height, $weight);
+echo "</div>";
+?>
+
