e7e737006b7a9b06eff2a39b1af5873a7775065f
kancheng <zxdfgcv@gmail.com>
2018-12-13 11:35:40 +0800

    Upload ch7 book exercise 15

---
+++ string-pra/phpFormatStr2/phpFormatStr2.php
@@ -0,0 +1,10 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$dollar = array( "1", "0.0322", "0.0210", "3.5252");
+
+	vprintf( "今日匯率 : %5.4f 臺幣(NTD) = %5.4f 美元(USD) = %5.4f 歐元(EUR) = %5.4f 日圓(YEN)", $dollar);
+
+echo "</div>";
+?>
+
