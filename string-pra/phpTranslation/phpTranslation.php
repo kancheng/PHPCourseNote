23faa419c96ae1d778a5a3c34a9617d0e8755692
kancheng <zxdfgcv@gmail.com>
2018-12-13 16:08:57 +0800

    Upload ch7 book exercise 19

---
+++ string-pra/phpTranslation/phpTranslation.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+	$str = "Horst <font color = 'blue'> Kan </font>";
+
+	echo $str . "<br />";
+
+	echo htmlspecialchars($str);
+
+echo "</div>";
+?>
+
