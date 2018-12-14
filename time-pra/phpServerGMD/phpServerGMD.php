5292449fa50a9279a900afd0f30c427de59f891f
kancheng <zxdfgcv@gmail.com>
2018-12-14 16:22:24 +0800

    Upload ch8 book exercise 2

---
+++ time-pra/phpServerGMD/phpServerGMD.php
@@ -0,0 +1,15 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Time</h1><hr />";
+
+$date_server = date("Y-m-d H:i:s");
+
+echo "Server Time : $date_server<br />";
+
+$date_gmt = gmdate("Y-m-d H:i:s");
+
+echo "GMT Time : $date_gmt<br />";
+
+
+echo "</div>";
+?>
+
