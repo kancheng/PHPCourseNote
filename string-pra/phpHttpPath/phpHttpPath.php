782ce578c61763662cf72ff4a13d64e6a0cd8093
kancheng <zxdfgcv@gmail.com>
2018-12-14 11:58:48 +0800

    Upload ch7 book exercise 25

---
+++ string-pra/phpHttpPath/phpHttpPath.php
@@ -0,0 +1,13 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Email</h1><hr />";
+
+	$strURL = "URL : https://www.google.com";
+
+	$regex = "/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/";
+
+	echo preg_replace($regex,"<a href=\"\\0\" target=\"_blank\">\\0</a>",$strURL);
+
+echo "</div>";
+
+?>
+
