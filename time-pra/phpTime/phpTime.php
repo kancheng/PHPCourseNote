14e7754ee050bfa64532a875ac4ed9a206e0ee38
kancheng <zxdfgcv@gmail.com>
2018-12-14 14:59:49 +0800

    Upload ch8 book exercise 1

---
+++ time-pra/phpTime/phpTime.php
@@ -0,0 +1,27 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Time</h1><hr />";
+
+echo "getdate() Function <hr />";
+
+$nowTime = getdate();
+
+foreach ($nowTime as $key => $value) {
+	echo "$key => $value <br />";
+}
+
+echo "<hr /> localtime( time(), 1) Function<hr />";
+$nowTime = localtime( time(), 1);
+foreach ($nowTime as $key => $value) {
+	echo "$key => $value <br />";
+}
+
+
+echo "<hr /> gettimeofday() Function<hr />";
+$nowTime = gettimeofday();
+foreach ($nowTime as $key => $value) {
+	echo "$key => $value <br />";
+}
+
+echo "</div>";
+?>
+
