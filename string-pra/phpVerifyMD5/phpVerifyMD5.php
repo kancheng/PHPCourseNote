47a91bb59fca0c0e75c09ad1f0f3ee4f2f633bbf
kancheng <zxdfgcv@gmail.com>
2018-12-14 11:43:32 +0800

    Upload ch7 book exercise 23

---
+++ string-pra/phpVerifyMD5/phpVerifyMD5.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+echo "<div style = 'text-align:center'><h1>String</h1><hr />";
+
+if(isset($_POST["passwd"]) && $_POST["passwd"]!=""){
+
+	$passStr = md5( "chiu1030");	
+	$inputStr = $_POST["passwd"];
+	if(md5($inputStr)==$passStr){
+		echo "Passowrd $inputStr Pass.<br />";
+		echo "<a href='#' onclick='window.history.back();';>回上一頁</a>";
+	}else{
+		echo "Password $inputStr Error.<br />";	
+		echo "<a href='#' onclick='window.history.back();';>回上一頁</a>";
+	}
+} else {
+
+?>
+	<form action="" name="form1" method="POST">
+		Passowrd :
+		<input name="passwd" type="password" id="passwd">
+		<input type="submit" name="Submit" value="Verify">
+	</form>
+
+<?php
+
+}
+echo "</div>";
+?>
