68075753454d620a44be92120e1b41c56bfcc093
kancheng <zxdfgcv@gmail.com>
2018-12-14 17:23:54 +0800

    Upload ch8 book exercise 6

---
+++ time-pra/phpCheckDay/phpCheckDay.php
@@ -0,0 +1,15 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Time</h1><hr />";
+## Leap Year
+
+$thisYear = date("Y");
+
+	if(checkdate(2,29, $thisYear)) {
+			echo "Is a leap year. $thisYear";
+		} else {
+			echo "Not a leap year. $thisYear";
+	}
+
+echo "</div>";
+?>
+
