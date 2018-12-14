5a119d408af764396999c5a04515ffd1ace76839
kancheng <zxdfgcv@gmail.com>
2018-12-14 11:53:07 +0800

    Upload ch7 book exercise 24

---
+++ string-pra/phpEmail/phpEmail.php
@@ -0,0 +1,14 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Email</h1><hr />";
+
+	$strMail = "as@phptest.com";
+ 
+	if (preg_match ("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/",$strMail,$strMatchData)) {
+		echo "Pass，E-mail : {$strMatchData[0]} .";
+	} else {
+		echo "Error .";
+	}
+
+echo "</div>";
+?>
+
