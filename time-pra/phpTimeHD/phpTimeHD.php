94b3a9800bf5bee53074241d4410889e22245cda
kancheng <zxdfgcv@gmail.com>
2018-12-14 16:45:11 +0800

    Upload ch8 book exercise 4

---
+++ time-pra/phpTimeHD/phpTimeHD.php
@@ -0,0 +1,18 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Time</h1><hr />";
+
+$datastr = "1999-10-30 00:13:14";
+
+sscanf($datastr, "%d-%d-%d %d:%d:%d", $y, $m, $d, $h, $i, $s);
+
+$timestamp1 = mktime($h, $i, $s, $m, $d, $y);
+
+echo $timestamp1 . "<br />";
+
+$timestamp2 = strtotime($datastr);
+
+echo $timestamp2;
+
+echo "</div>";
+?>
+
