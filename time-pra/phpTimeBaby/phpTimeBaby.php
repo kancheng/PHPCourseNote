c385bc19053caaf20c05b2bff7b60034d762f5b9
kancheng <zxdfgcv@gmail.com>
2018-12-14 17:07:51 +0800

    Upload ch8 book exercise 5

---
+++ time-pra/phpTimeBaby/phpTimeBaby.php
@@ -0,0 +1,18 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Time</h1><hr />";
+
+$datastr = "1999-10-30 00:13:14";
+
+$timestamp = strtotime($datastr);
+
+echo "出生時間 : " . $datastr . "<br />";
+
+echo "懷孕日期(推算) : " . date("Y-m-d", strtotime("- 10 months", $timestamp)) . "<br />";
+
+echo "滿月日期(推算) : ". date("Y-m-d", strtotime("+ 1 months", $timestamp)) . "<br />";
+
+echo "周歲日期(推算) : ". date("Y-m-d", strtotime("+ 1 year", $timestamp)) . "<br />";
+
+echo "</div>";
+?>
+
