8de7aad3f1f2c354bccbdb3659cbe93def367f63
kancheng <zxdfgcv@gmail.com>
2018-12-14 16:34:20 +0800

    Upload ch8 book exercise 3

---
+++ time-pra/phpFTime/phpFTime.php
@@ -0,0 +1,14 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>Time</h1><hr />";
+
+echo strftime("%A, %d %b %Y %H:%M:%S") . "<br />";
+echo gmstrftime("%A, %d %b %Y %H:%M:%S") . "<br />";
+
+setlocale(LC_ALL, 'cht_taiwan');
+
+echo mb_convert_encoding(strftime("%A, %d %b %Y %H:%M:%S"), "UTF-8", "BIG5"). "<br />";
+echo mb_convert_encoding(gmstrftime("%A, %d %b %Y %H:%M:%S"),"UTF-8", "BIG5") . "<br />";
+
+echo "</div>";
+?>
+
