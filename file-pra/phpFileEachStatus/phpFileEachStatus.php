c15b700e475b8a742bf52d9d0b8f3e36e11ab9ae
kancheng <zxdfgcv@gmail.com>
2018-12-17 14:54:42 +0800

    Upload ch9 book exercise 5

---
+++ file-pra/phpFileEachStatus/phpFileEachStatus.php
@@ -0,0 +1,28 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>File</h1><hr />";
+
+$path = __FILE__;
+
+clearstatcache();
+
+echo fileinode($path) . "<br />";
+
+echo fileperms($path) . "<br />";
+
+echo fileowner($path) . "<br />";
+
+echo filegroup($path) . "<br />";
+
+echo filesize($path) . "<br />";
+
+echo fileatime($path) . "<br />";
+
+echo filemtime($path) . "<br />";
+
+echo filectime($path) . "<br />";
+
+echo filetype($path) . "<br />";
+
+echo "</div>";
+?>
+
