1f222766ba18ead62444e8ab0ddba56ac696eaf8
kancheng <zxdfgcv@gmail.com>
2018-12-17 14:41:30 +0800

    Upload ch9 book exercise 3

---
+++ file-pra/phpPathInfo/phpPathInfo.php
@@ -0,0 +1,20 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>File</h1><hr />";
+
+$path = __FILE__;
+
+echo pathinfo( $path, PATHINFO_DIRNAME). "<br />";
+
+echo pathinfo( $path, PATHINFO_BASENAME). "<br />";
+
+echo pathinfo( $path, PATHINFO_EXTENSION). "<br />";
+
+echo pathinfo( $path, PATHINFO_FILENAME). "<br />";
+
+$pathData = pathinfo($path);
+
+print_r($pathData);
+
+echo "</div>";
+?>
+
