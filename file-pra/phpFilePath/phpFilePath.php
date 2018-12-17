c6a7d1d83e24450f12996c1371a85babaacefad2
kancheng <zxdfgcv@gmail.com>
2018-12-17 11:50:14 +0800

    Upload ch9 book exercise 1

---
+++ file-pra/phpFilePath/phpFilePath.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>File</h1><hr />";
+
+echo "相對路徑 : " . $_SERVER["PHP_SELF"] . "<br />";
+
+echo "網路上的絕對路徑 : " . $_SERVER["SCRIPT_FILENAME"] . "<br />";
+
+echo "顯示檔案實際路徑 : " . __FILE__;
+
+echo "</div>";
+?>
+
