df1c08c1b56c31973337f719fdf32ea8721023ec
kancheng <zxdfgcv@gmail.com>
2018-12-17 15:04:41 +0800

    Upload ch9 book exercise 6

---
+++ file-pra/phpDiskSpace/phpDiskSpace.php
@@ -0,0 +1,11 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>File</h1><hr />";
+
+printf(" 磁碟空間 : %s bytes <br /> 剩餘空間 : %s bytes .", 
+	number_format( disk_total_space(".")), 
+	number_format( disk_free_space("."))
+);
+
+echo "</div>";
+?>
+
