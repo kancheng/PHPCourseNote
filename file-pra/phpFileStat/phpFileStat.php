4b1b06cc58d3b3f720fb47373fb5012d2b01a9de
kancheng <zxdfgcv@gmail.com>
2018-12-17 14:48:34 +0800

    Upload ch9 book exercise 4

---
+++ file-pra/phpFileStat/phpFileStat.php
@@ -0,0 +1,14 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>File</h1><hr />";
+
+$path = __FILE__;
+
+clearstatcache();
+
+$pathData = stat($path);
+
+print_r($pathData);
+
+echo "</div>";
+?>
+
