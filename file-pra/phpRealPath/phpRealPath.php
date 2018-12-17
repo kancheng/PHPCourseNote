74acee780e444003134e8c050232d8311193dec5
kancheng <zxdfgcv@gmail.com>
2018-12-17 12:02:05 +0800

    Upload ch9 book exercise 2

---
+++ file-pra/phpRealPath/phpRealPath.php
@@ -0,0 +1,14 @@
+﻿<?php
+echo "<div style = 'text-align:center'><h1>File</h1><hr />";
+
+echo "目前檔案所在路徑 : " . realpath(".") . "<br />";
+
+echo "完整的檔案路徑 : " . __FILE__ . "<br />";
+
+echo "目錄路徑為 : " . __DIR__ . "<br />";
+
+echo "檔案名稱 : " . basename(__FILE__);
+
+echo "</div>";
+?>
+
